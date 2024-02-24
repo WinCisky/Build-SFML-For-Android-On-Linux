1da09627e552e2f7b8312eac195d74bc24778c3e
Simone <xsimone97@hotmail.it>
2024-02-24 15:22:14 +0100

    flag fix and sequential build
    only arm64-v8a abi is builded

--- rebuild-all.sh
+++ rebuild-all.sh
@@ -2,13 +2,10 @@
 
 readonly CURRENT_PATH=$(dirname "$(readlink -f "$0")")
 
-abis=(x86 armeabi-v7a arm64-v8a x86_64)
+# abis=(x86 armeabi-v7a arm64-v8a x86_64)
+abis=(arm64-v8a)
 for abi in ${abis[@]} ; do
     pushd ${CURRENT_PATH}/${abi}
-    if type gnome-terminal > /dev/null 2>&1; then
-        gnome-terminal --tab -e "/bin/bash -c '${CURRENT_PATH}/${abi}/rebuild.sh; exec /bin/bash -i'" # The terminal should not be closed after the script finished running.
-    else
-        ${CURRENT_PATH}/${abi}/rebuild.sh
-    fi
+    ${CURRENT_PATH}/${abi}/rebuild.sh
     popd
 done
